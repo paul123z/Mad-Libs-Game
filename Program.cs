dda9fa80b7925be6a2426efbbd96f7cbac3272e1
Pavol Lantaj <palolantaj@gmail.com>
2022-07-12 19:53:41 +0200

    Final Push

--- Program.cs
+++ Program.cs
@@ -10,6 +10,22 @@ namespace Mad_Libs_Game
     {
         static void Main(string[] args)
         {
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color:");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun:");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity:");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are {0}",color);
+            Console.WriteLine("{0} are blue",pluralNoun);
+            Console.WriteLine("I Love {0}",celebrity);
+
+            //Console.ReadLine();
         }
     }
 }
